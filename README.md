0f876265135d6e6148c83fcb0a97edbe7ddee54a
21Alul21 <augustine.alul@yahoo.com>
2024-07-02 07:13:39 +0100

    location api code

---
+++ README.md
@@ -0,0 +1,5 @@
+# HNG INTERNSHIP
+
+This directory contains all the tasks I did during the HNG 11 internship
+
+---------------------------
---
+++ app.py
@@ -0,0 +1,53 @@
+""" module containing an api endpoint that response only to a get request,
+it utilizes the `requests` library for making requests to other third party
+api endpoints 
+"""
+
+import requests
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/api/hello', methods=['GET'])
+def visitor_info():
+	"""View for handling the request including a querry parameter
+	"""
+
+	client_ip = request.headers.get('X-Requested-For')
+	if client_ip:
+		client_ip = client_ip.split(',')[0]
+	if not client_ip:
+		client_ip = request.remote_addr
+
+	visitor_name = request.args.get('visitor_name')
+
+	location_api_url = 'http://ip-api.com/json/' + str(client_ip)
+	print(location_api_url)
+	location_response =  requests.get(location_api_url)
+	if location_response.status_code == 200:
+		location_data = location_response.json()
+		print(location_data)
+	else:
+		return jsonify({"error": "your city was not loaded successfully"})
+
+	location = location_data.get('city') 
+
+	weather_api_url = f'http://api.weatherapi.com/v1/current.json?key=31d8e3e2f00a4cd1be0135729240107&q={location}'
+	weather_response = requests.get(weather_api_url) 
+	if weather_response.status_code == 200:
+		weather_data = weather_response.json()
+		temperature = weather_data.get('current')['temp_c']
+	
+	
+
+	return jsonify({'client_ip': client_ip,
+			'location': location,
+			'greeting': f'Hello, {visitor_name}!, the temperature is\
+			{temperature} degrees celcius in {location}'
+	})
+
+if __name__ == '__main__':
+	app.run() 
+
+
